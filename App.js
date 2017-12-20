34b242eee05c93c197047a03ca404ae9795134c7
노경모 <kyeongmo2@gmail.com>
2017-12-20 17:52:12 +0900

    apply expo vector icons & weather api

--- App.js
+++ App.js
@@ -1,29 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import {
+  StyleSheet, Text, View, ActivityIndicator,
+  StatusBar
+} from 'react-native';
 import Weather from './Weather';
+
+const API_KEY = '';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoaded: true
+      isLoaded: false,
+      error: null,
+      temperature: null,
+      name: null
     };
+
+    this._getWeather = this._getWeather.bind(this);
+  }
+
+  _getWeather(lat, lon) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`)
+      .then((response) => response.json())
+      .then((data) => this.setState({
+        isLoaded: true,
+        temperature: data.main.temp,
+        name: data.weather[0].main
+      }))
   }
 
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error } = this.state;
 
     return (
       <View style={styles.container}>
+        <StatusBar hidden={true}/>
         {
           isLoaded ?
             <Weather />
             :
             <View style={styles.loading}>
               <Text style={styles.loadingText}>Getting the weather</Text>
+              {
+                error ?
+                  <Text style={styles.errorText}>{error}</Text>
+                  :
+                  null
+              }
             </View>
         }
       </View>
+    );
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this._getWeather(position.coords.latitude, position.coords.longitude);
+      },
+      (error) => this.setState({error})
     );
   }
 }
@@ -32,6 +68,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff'
+  },
+  errorText: {
+    color: 'red',
+    backgroundColor: 'transparent',
+    marginBottom: 40
   },
   first: {
     flex: 1,
